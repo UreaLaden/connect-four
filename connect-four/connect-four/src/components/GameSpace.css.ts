41dafb8ae24e15fb16fb1da38a57c525b85c955c
Leaundrae Mckinney <lmckinney@microsoft.com>
2023-01-06 11:09:57 -0500

    Initial grid for game space

--- connect-four/connect-four/src/components/GameSpace.css.ts
+++ connect-four/connect-four/src/components/GameSpace.css.ts
@@ -1,5 +1,52 @@
 import {mergeStyleSets} from "@fluentui/react";
+import { Colors } from "../utils/colors";
 
 export const styles = mergeStyleSets({
-    
+    gameSpaceContainer:{
+        display:"grid",
+        "grid-template-columns": "1fr 2fr 1fr",
+    },
+    mainContainer:{
+        height:"100%",
+        marginTop:"2%"
+    },
+    menuButtonOne:{
+        "grid-column-start":0,
+        "grid-column-end":1,
+        backgroundColor:Colors.Pink,
+        border:"1px solid black"
+    },
+    menuButtonTwo:{
+        "grid-column-start":2,
+        "grid-column-end":3,
+        backgroundColor:Colors.Gold,
+        border:"1px solid black"
+    },
+    logo:{
+        "grid-column-start":1,
+        "grid-column-end":2,
+        backgroundColor:Colors.Gold,
+        border:"1px solid black",
+        height:"5vh"
+    },
+    homePlayer:{
+        border:"1px solid black"
+    },
+    awayPlayer:{
+        border:"1px solid black"
+    },
+    playerTurnMarker:{
+        border:"1px solid black"
+    },
+    gameBoard:{
+        border:"1px solid black",
+        height:"70vh"
+    },
+    backgroundImage:{
+        backgroundColor:Colors.DarkMagenta,
+        height:"25vh",
+        borderRadius:"25px 25px 0px 0px",
+        justifySelf:"stretch",
+        gridColumn:"1 / -1"
+    }
 })
--- connect-four/connect-four/src/components/GameSpace.tsx
+++ connect-four/connect-four/src/components/GameSpace.tsx
@@ -1,8 +1,21 @@
+import { styles } from "./GameSpace.css";
 
-const GameSpace = (props:any) => {
+const GameSpace = (props: any) => {
     //Probably best to use css grid for the position
     return (
-        <div>Game Space</div>
+        <div className={styles.mainContainer}>
+
+        <div className={styles.gameSpaceContainer}>
+            <div className={styles.menuButtonOne}>MenuButton</div>
+            <div className={styles.logo}>Logo</div>
+            <div className={styles.menuButtonTwo}>ResetButton</div>
+            <div className={styles.homePlayer}>HomePlayer</div>
+            <div className={styles.gameBoard}>Play Area</div>
+            <div className={styles.awayPlayer}>AwayPlayer</div>
+            <div className={styles.backgroundImage}>Background Image</div>
+        </div>
+            <div className={styles.playerTurnMarker}>Turn Marker</div>
+        </div>
     )
 }
 
